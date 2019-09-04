099ae8c165aeb5b875ec1f8de9074ff77aed7098
Thibault Marin <thibault@ibault.com>
2019-09-04 15:19:52 +0200

    Frontend First

---
+++ components/Quotes.vue
@@ -0,0 +1,14 @@
+<template>
+  <resource-grid v-bind="$props">
+    <template>
+        <h1>test</h2>
+    </template>
+  </resource-grid>
+</template>
+
+<script>
+export default {
+  name: 'Quotes',
+  components: { Quotes },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,28 @@
     </site-hero>
     <main-section theme="one-column">
       <template v-slot:default>
+        <h6>Featured Jobs</h6>
         <!-- All Posts -->
         <posts-grid />
+        <h6>Frontend First</h6>
+        <div class="container-center">
+          <p>
+            At Frontend Guild we are specialized in Frontend and Node.js
+            development. We distinguish ourselves from other staffing companies
+            by being experts in our discipline. We strongly believe that you
+            can’t be an expert in everything because of that we focus only on
+            JavaScript related technologies. We are proud that we talk the same
+            language as developers, which makes it more easy to understand the
+            needs and expectations of hiring managers and frontend engineers.
+          </p>
+          <div class="logo-container">
+            <img src="/javascript.svg" alt="javascript logo" />
+            <img src="/vue.svg" alt="vue logo" />
+            <img src="/angular.svg" alt="angular logo" />
+            <img src="/react.svg" alt="react logo" />
+            <img src="/node.svg" alt="node logo" />
+          </div>
+        </div>
       </template>
       <template v-slot:sidebar>
         Nothing here
