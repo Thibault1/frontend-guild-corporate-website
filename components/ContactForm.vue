76a75094bbe4b807600fa6fa6842366ad01c788e
Thibault Marin <thibault@ibault.com>
2019-10-10 15:16:23 +0700

    update form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,27 +1,51 @@
 <template>
   <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your Name: <input type="text" name="name"/></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email"/></label>
-    </p>
-    <p>
-      <label
-        >Your Role:
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label
-      >
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">
-        Send
+    <!-- First name -->
+    <div class="field">
+      <label class="label is-medium">Name *</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="text" name="name" required />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="user" />
+        </span>
+      </div>
+    </div>
+    <!-- Email -->
+    <div class="field">
+      <label class="label is-medium">Email *</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="email" name="email" required />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="envelope" />
+        </span>
+      </div>
+    </div>
+    <!-- CV -->
+    <div class="field">
+      <label class="label is-medium">CV</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="file" name="cv" />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="briefcase" />
+        </span>
+      </div>
+    </div>
+    <!-- Message -->
+    <div class="field">
+      <label class="label is-medium">Your message</label>
+      <div class="control">
+        <textarea
+          class="textarea"
+          name="message"
+          placeholder="Please tell us how can we help you."
+          rows="8"
+        ></textarea>
+      </div>
+    </div>
+    <div class="control">
+      <button class="button is-primary" type="submit">
+        Contact Us
       </button>
-    </p>
+    </div>
   </form>
 </template>
