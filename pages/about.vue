22b8a03b0512fedc75c45bfd9f1a77b2a54c149d
Thibault Marin <thibaultmarin@Thibaults-MacBook-Pro.local>
2020-10-29 07:26:02 +0100

    Edit about page

--- pages/about.vue
+++ pages/about.vue
@@ -8,32 +8,32 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Why Frontend Guild</h3>
+          <h3>We provide External Software Engineers.</h3>
           <p>
-            Frontend Guild was founded by IT Experts and is a company for top
-            developers, producing high-quality code with exceptional user
-            experience.
+            Modern enterprises rely heavily on skilled IT Engineers, we can help
+            you with additional capacity for your software development teams.
           </p>
           <p>
             Frontend Guild is a specialized consultancy company for all your
-            software development needs. Our primary aim is to provide the
-            Belgian Tech Industry with capable and motivated Engineers.
+            software development needs, we provide the Belgian Tech Industry
+            with capable and motivated Engineers.
           </p>
 
-          <h3>How do we do it?</h3>
+          <p>
+            Our Software Engineers are ready to collaborate with your team to
+            create applications and services that stand out from the competition
+            and support you with your digital transformations journeys.
+          </p>
           <p>
             As a company, we have a very rigorous screening process to identify
-            the best candidates. We do in-house technical screenings with all
-            our candidates, pair programming tests are an essential part of our
-            recruitment process.
+            and attract the best engineers. We do in-house technical screenings
+            with all our candidates, pair programming tests are an essential
+            part of our recruitment process.
           </p>
           <p>
-            So we know for sure the CV from the developer matches his/her skill
-            set. We are experts in our domain, and we look for great problem
-            solvers with passion and drive — the types of people we want to work
-            with.
+            We are experts in our domain, and we look for great problem solvers
+            with passion and drive — the types of people we want to work with.
           </p>
-
           <p>
             At Frontend Guild, the developer comes first: we always think about
             the best work-life balance possible for our Engineers.
